82ae3b2af6c4ada1ae8027109f11fa994df09522
linhvodanh2004 <linhvodanh2004@gmail.com>
2025-05-17 14:34:27 +0700

    Cut components and modify UI

--- shopapp-angular/src/app/home/home.component.ts
+++ shopapp-angular/src/app/home/home.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-
+export class HomeComponent { 
 }
